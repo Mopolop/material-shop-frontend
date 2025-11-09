969f0a2674cf95fb4cadf160a28a411677e3b778
Mopolop <knyazevam@student.bmstu.ru>
2025-11-10 00:55:33 +0300

    deploy

--- material_store/src/App.tsx
+++ material_store/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { MaterialsPage } from "./pages/MaterialsPage";
 import { MaterialDetailPage } from "./pages/MaterialDetailPage";
@@ -7,16 +7,15 @@ import { Header } from "./components/Header";
 
 export const App: React.FC = () => {
   return (
-    <Router>
+    <BrowserRouter basename="/material-shop-frontend"> 
       <Header />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} index />
         <Route path="/catalog" element={<MaterialsPage />} />
         <Route path="/detailed_material/:id" element={<MaterialDetailPage />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 };
-
 
 export default App;
--- material_store/vite.config.ts
+++ material_store/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/material-shop-frontend",
   server: {
     port: 3000,
     proxy: {
